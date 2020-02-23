21c0a8a87b9fa4e86456d13f1011818d5139250e
Ankit Kumar <54996872+AnkitKumar1311@users.noreply.github.com>
2020-02-24 00:01:24 +0530

    Add files via upload

---
+++ Inheritance in CPP/Multiple Inheritance.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<conio.h>
+#include<cstdlib>
+
+using namespace std;
+
+class ParentClass
+{
+        int num;
+    protected :
+        string name;
+    public:
+        ParentClass() {this->num=0;this->name="NA";cout<<"\n Parent CTOR";}
+
+        ParentClass (int sk, string name) {
+            cout<<"\n Parent CTOR";
+            this->num=sk;
+            this->name = name;
+        }
+
+        void display()
+        {
+            cout<<"\n Number = "<<this->num<<"\nName="<<this->name;
+        }
+};
+
+class SecondClass {
+    public :
+        SecondClass ()
+        {
+            cout<<"\n Second parent class CTOR called";
+        }
+};
+
+class DerivedClass : private ParentClass, public SecondClass
+{
+        int dnum;
+        ParentClass ob;
+
+    public :
+
+        DerivedClass () {
+            cout<<"\n Derived CTOR";
+        }
+
+        DerivedClass (int num, string name, int dnum) : ParentClass(num, name) {
+            cout<<"\n Derived class CTOR";
+            this->dnum=dnum;
+        }
+
+        void display()
+        {
+            this->ParentClass::display();
+            cout<<"\n Dnum = "<<this->dnum;
+        }
+
+};
+
+int main()
+{
+    DerivedClass ob(1,"ankit", 2);
+    ob.display();
+    return 0;
+}
+
